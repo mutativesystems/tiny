eb5f4a811c76eda136f11700d57bdd4d0ec3bd65
mutativesystems <>
2025-06-05 06:09:42 +0700

    god css is stupid

--- index.js
+++ index.js
@@ -1,20 +1,25 @@
-function createCarouselEntry(nameText, heightInt) {
+function createCarouselEntry({ name, height, artworkSrc, artworkScaling }) {
   const entry = document.createElement("div");
   entry.className = "carousel-entry";
 
   const heightDiv = document.createElement("div");
   heightDiv.className = "student-height";
-  heightDiv.textContent = heightInt.toString();
+  heightDiv.textContent = height.toString();
 
   const nameDiv = document.createElement("div");
   nameDiv.className = "student-name";
-  nameDiv.textContent = nameText;
+  nameDiv.textContent = name;
 
   const fillDiv = document.createElement("div");
   fillDiv.className = "height-fill";
-  fillDiv.style.height = `${Math.round(480 * heightInt / 230)}px`;
+  fillDiv.style.height = `${Math.round(480 * height / 230)}px`;
 
   const image = document.createElement("img");
+  image.className = "student-artwork";
+  image.src = artworkSrc;
+  image.style.height = `${Math.round(480 * height * artworkScaling / 230)}px`;
+
+  fillDiv.appendChild(image);
 
   entry.appendChild(nameDiv);
   entry.appendChild(heightDiv);
@@ -26,6 +31,6 @@ function createCarouselEntry(nameText, heightInt) {
 const carousel = document.querySelector("#carousel");
 
 for (student of students) {
-  const c = createCarouselEntry(student.name, student.height);
+  const c = createCarouselEntry(student);
   carousel.appendChild(c);
 }
